04bfc3202808e71cb7ac90523c6126043b02bdff
Kumaraguru <19eucs071@skcet.ac.in>
2021-11-20 01:02:19 +0530

    fixing navbar states

--- components/SidebarMobile.js
+++ components/SidebarMobile.js
@@ -1,42 +1,41 @@
 import { Button } from "@chakra-ui/button";
 import React from "react";
 import { Component } from "react";
-import './test.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import "./test.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class SidebarMobile extends Component {
-
   constructor(props) {
     super(props);
-    this.state = { ClickedButton: ''};
+    this.state = { ClickedButton: "" };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(id) {
-
-    this.setState({ClickedButton: id})
-    this.props.selectedtype.bind(this, id)()
+    this.setState({ ClickedButton: id });
+    this.props.selectedtype.bind(this, id)();
   }
   render() {
-  
-    return  (
+    return (
       <div class="bg-indblue static uppercase font-semibold  flex md:flex-col  shadow-lg  justify-around md:h-screen md:w-60 fixed w-screen bottom-0 items-center">
-      <img
-        className="hidden h-10 md:block"
-        src="https://mic.gov.in/assets/img/logo.png"
-      ></img>
-      {
-        this.props.moduletypes.map((moduletype, selectedtype) =>
-
-        <div className={moduletype.id === this.state.ClickedButton ? "bg-white md:p-5  md:flex md:flex-wrap md:p-5 p-3 delay-75 duration-500 ease-in-out transform hover:scale-125 items-center" : "transparent md:flex md:flex-wrap md:p-5 p-3 delay-75 duration-500 ease-in-out transform hover:scale-125 items-center"}
-        id={moduletype.id} 
-        key={moduletype.id}
-        onClick={ () => this.handleChange(moduletype.id)}
-        >
-         
-         <FontAwesomeIcon icon="fa-house"/> 
-        {/* <svg
+        <img
+          className="hidden h-10 md:block"
+          src="https://mic.gov.in/assets/img/logo.png"
+        ></img>
+        {this.props.moduletypes.map(
+          (moduletype, selectedtype) => (
+            <div
+              className={
+                moduletype.id === this.state.ClickedButton
+                  ? "bg-white md:p-5  md:flex md:flex-wrap md:p-5 p-3 delay-75 duration-500 ease-in-out transform hover:scale-125 items-center text-indblue rounded-xl"
+                  : "transparent md:flex md:flex-wrap md:p-5 p-3 delay-75 duration-500 ease-in-out transform hover:scale-125 items-center text-white"
+              }
+              id={moduletype.id}
+              key={moduletype.id}
+              onClick={() => this.handleChange(moduletype.id)}
+            >
+              <FontAwesomeIcon icon="fa-house" />
+              {/* <svg
           xmlns="http://www.w3.org/2000/svg"
           class="text-white h-6 w-6 md:mr-3"
           fill="none"
@@ -50,31 +49,26 @@ class SidebarMobile extends Component {
             d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
           />
         </svg> */}
-        <span
-          style={{ fontFamily: "Montserrat" }}
-          className="hidden text-white md:block text-sm"
-        >
-         {moduletype.type}
-        </span>
-      </div>
+              <span
+                style={{ fontFamily: "Montserrat" }}
+                className="hidden  md:block text-sm"
+              >
+                {moduletype.type}
+              </span>
+            </div>
+          )
 
-      //   <input type="button"
-      //   className= {moduletype.id === this.state.ClickedButton ? "bg-indblue" : "bg-black"}
-      //   style={{width: '25%', border: "none"}} 
-      //   id={moduletype.id} 
-      //   key={moduletype.id} 
-      //   value={ moduletype.type} 
-      //   onClick={ () => this.handleChange(moduletype.id)}
-      //  />
-        
-        )
-      }
-         
+          //   <input type="button"
+          //   className= {moduletype.id === this.state.ClickedButton ? "bg-indblue" : "bg-black"}
+          //   style={{width: '25%', border: "none"}}
+          //   id={moduletype.id}
+          //   key={moduletype.id}
+          //   value={ moduletype.type}
+          //   onClick={ () => this.handleChange(moduletype.id)}
+          //  />
+        )}
       </div>
-   
-  
     );
-     
   }
 }
 
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,105 +1,133 @@
-import React , { Component } from 'react';
-import EditProfile from '../components/EditProfile';
-import News from '../components/News';
-import SidebarMobile from '../components/SidebarMobile';
+import React, { Component } from "react";
+import EditProfile from "../components/EditProfile";
+import News from "../components/News";
+import SidebarMobile from "../components/SidebarMobile";
 import { WelcomeHero } from "../components/WelcomeHero";
 
-
 class Dashboard extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.setModuleType = this.setModuleType.bind(this);
+  constructor(props) {
+    super(props);
+    this.setModuleType = this.setModuleType.bind(this);
+  }
 
-    }
-    
+  state = {
+    moduletypes: [
+      {
+        id: 1,
+        type: "Home",
+      },
+      {
+        id: 2,
+        type: "Project",
+      },
+      {
+        id: 3,
+        type: "Resources",
+      },
+      {
+        id: 4,
+        type: "Profile",
+      },
+    ],
+    selectedmoduletype: 0,
+  };
 
-   state = {
-       moduletypes: [
-           {
-               id: 1,
-               type: 'Home',
-               
-           },
-           {
-               id: 2,
-               type: 'Project'
-           },
-           {
-               id: 3,
-               type: 'Resources'
-           },
-           {
-               id: 4,
-               type: 'Profile'
-
-           }
-       ],
-       selectedmoduletype:0,
-   }
-   
-   setModuleType(selectedtype) {
-       
-        this.setState({selectedmoduletype : selectedtype})
-   }
-   render() {
-       if(this.state.selectedmoduletype === 1){
-        return(
-            <>
-            <SidebarMobile moduletypes={ this.state.moduletypes } selectedtype={this.setModuleType}/>
-             <div className="flex flex-col min-h-screen">
+  setModuleType(selectedtype) {
+    this.setState({ selectedmoduletype: selectedtype });
+  }
+  render() {
+    if (this.state.selectedmoduletype === 1) {
+      return (
+        <>
+          <SidebarMobile
+            moduletypes={this.state.moduletypes}
+            selectedtype={this.setModuleType}
+          />
+          <div className="flex flex-col min-h-screen">
             <div className="flex  flex-wrap">
-        <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
-          <WelcomeHero h1="Welcome, " h2="Bhuvanesh." />
-          <News />
-        </div>
-      </div>
-    </div>
-              
-            </>
-           );
-        }
-        if(this.state.selectedmoduletype === 2){
-            return(
-                <SidebarMobile moduletypes = { this.state.moduletypes } selectedtype={ this.setModuleType } />
-            );
-        }
-        if(this.state.selectedmoduletype === 3){
-            return(
-                <>
-                 <SidebarMobile moduletypes = { this.state.moduletypes } selectedtype={ this.setModuleType } />
-                <div className="flex flex-col min-h-screen">
-                     <div className="flex  flex-wrap">
-                         <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
-                             <WelcomeHero h1="RESOURCES" />
-                          </div>
-                          </div>
-                         </div>
-            </>
-            );
-        }
-        if(this.state.selectedmoduletype === 4){
-            return(
-                <>
-                <SidebarMobile moduletypes = { this.state.moduletypes } selectedtype={ this.setModuleType } />
-                <div className="flex flex-col min-h-screen">
-                     <div className="flex  flex-wrap">
-                         <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
-                              <WelcomeHero h1="PROFILE" />
-                              <EditProfile />
-                              </div>
-                              </div>
-                              </div>
-                </>
-                );
-            }
-        
-       else{
-           return(
-            <SidebarMobile moduletypes={ this.state.moduletypes } selectedtype={this.setModuleType}/>
-           );
-       }
-       
-   }
+              <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
+                <WelcomeHero h1="Welcome, " h2="Bhuvanesh." />
+                <News />
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
+    if (this.state.selectedmoduletype === 2) {
+      return (
+        <SidebarMobile
+          moduletypes={this.state.moduletypes}
+          selectedtype={this.setModuleType}
+        />
+      );
+    }
+    if (this.state.selectedmoduletype === 3) {
+      return (
+        <>
+          <SidebarMobile
+            moduletypes={this.state.moduletypes}
+            selectedtype={this.setModuleType}
+          />
+          <div className="flex flex-col min-h-screen">
+            <div className="flex  flex-wrap">
+              <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
+                <WelcomeHero h1="RESOURCES" />
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
+    if (this.state.selectedmoduletype === 4) {
+      return (
+        <>
+          <SidebarMobile
+            moduletypes={this.state.moduletypes}
+            selectedtype={this.setModuleType}
+          />
+          <div className="flex flex-col min-h-screen">
+            <div className="flex  flex-wrap">
+              <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
+                <WelcomeHero h1="PROFILE" />
+                <EditProfile />
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <SidebarMobile
+            moduletypes={this.state.moduletypes}
+            selectedtype={this.setModuleType}
+          />
+          <div className="flex flex-col min-h-screen">
+            <div className="flex  flex-wrap">
+              <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
+                <WelcomeHero h1="Welcome, " h2="Bhuvanesh." />
+                <div className="p-10 items-center md:ml-80">
+                  <div class="flex flex-wrap -mx-1 overflow-hidden lg:-mx-3">
+                    <div class="my-1 px-1 w-full overflow-hidden lg:my-3 lg:px-3">
+                      <img
+                        className="md:max-w-lg sm:max-w-md"
+                        src="assets/startup2.gif"
+                      />
+                    </div>
+
+                    <div class="my-1 px-1 w-full  overflow-hidden lg:my-3 lg:px-3">
+                      Welcome to the alumni portal. Navigate with the links in
+                      the menu.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
+  }
 }
 export default Dashboard;
