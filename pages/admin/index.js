7444417e25cdfd315995a311f49ac3a50e1d883a
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-02-15 13:03:28 +0530

    Refactor admin page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -13,10 +13,15 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faDatabase } from "@fortawesome/free-solid-svg-icons";
 import { faBookReader } from "@fortawesome/free-solid-svg-icons";
 import { faBullhorn } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faFileImport } from "@fortawesome/free-solid-svg-icons";
+import requireAuth from "../../utils/requireAuth";
+import Router from "next/router";
+import auth from "../../utils/auth";
+import Loading from "../../components/Loading";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-class Home extends Component{
+class Home extends Component {
   constructor(props) {
     super(props);
     this.setModuleType = this.setModuleType.bind(this);
@@ -27,238 +32,207 @@ class Home extends Component{
       {
         id: 1,
         type: "Home",
-        icon: faHome
+        icon: faHome,
       },
       {
         id: 2,
         type: "Data",
-        icon: faDatabase
+        icon: faDatabase,
       },
       {
         id: 3,
         type: "Resources",
-        icon: faBookReader
+        icon: faBookReader,
       },
       {
         id: 4,
         type: "ANNOUNCEMENTS",
-        icon: faBullhorn
+        icon: faBullhorn,
       },
       {
         id: 5,
-        type: "ADD USERS",
-        icon: faUser
-      }
-
+        type: "Import",
+        icon: faFileImport,
+      },
     ],
     selectedmoduletype: 1,
+    user: null,
+    isAuth: false,
+    userId: 0,
+    isLoading: true,
   };
+
+  async componentDidMount() {
+    let data = await requireAuth("/");
+
+    if (data.isAuth) {
+      if (data.user.role !== "admin") {
+        await Router.push("/dashboard");
+      }
+
+      this.setState({
+        user: data.user,
+        isAuth: data.isAuth,
+        userId: data.user.id,
+        logout: auth().logout,
+      });
+    }
+
+    this.setState({ isLoading: false });
+  }
 
   setModuleType(selectedtype) {
     this.setState({ selectedmoduletype: selectedtype });
   }
   render() {
+    if (this.state.isLoading) {
+      return <Loading> </Loading>;
+    }
+
     if (this.state.selectedmoduletype === 1) {
       return (
         <>
-       
-       
-       <div className="flex flex-col min-h-screen">
-       
-       <div className="flex  flex-wrap">
-        
-         
-       <AdminSidebar moduletypes={this.state.moduletypes} 
-          selectedtype={this.setModuleType}
-        />
-        <div className="flex flex-col bg-indblue min-h-full min-w-full">
+          <div className="flex flex-col min-h-screen">
             <div className="flex  flex-wrap">
-              <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
-              <WelcomeHero h1="Welcome, " h2="Admin." />
-           <AdminStats />
-           <ChartStats />
+              <AdminSidebar
+                moduletypes={this.state.moduletypes}
+                selectedtype={this.setModuleType}
+              />
+              <div className="flex flex-col bg-indblue min-h-full min-w-full">
+                <div className="flex  flex-wrap">
+                  <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
+                    <WelcomeHero h1="Welcome, " h2="Admin." />
+                    <AdminStats />
+                    <ChartStats />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-         {/* <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
-           <WelcomeHero h1="Welcome, " h2="Admin." />
-           <AdminStats />
-           <ChartStats />
-         </div> */}
-       </div>
-   </div>
-
-
-       
         </>
       );
     }
     if (this.state.selectedmoduletype === 2) {
       return (
-       
-           <>
-           
-           
-             <div className="flex flex-col min-h-screen">
-       
-       <div className="flex  flex-wrap">
-        
-         
-       <AdminSidebar moduletypes={this.state.moduletypes} 
-          selectedtype={this.setModuleType}
-        />
-        <div className="flex flex-col bg-indblue min-h-full min-w-full">
+        <>
+          <div className="flex flex-col min-h-screen">
             <div className="flex  flex-wrap">
-              <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
-              <WelcomeHero h1="User data" />
-          <DataComponent />
+              <AdminSidebar
+                moduletypes={this.state.moduletypes}
+                selectedtype={this.setModuleType}
+              />
+              <div className="flex flex-col bg-indblue min-h-full min-w-full">
+                <div className="flex  flex-wrap">
+                  <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
+                    <WelcomeHero h1="User data" />
+                    <DataComponent />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        
-       </div>
-   </div>
-
-
-       
         </>
-        
-        
       );
     }
     if (this.state.selectedmoduletype === 3) {
       return (
         <>
-              <div className="flex flex-col min-h-screen">
-       
-       <div className="flex  flex-wrap">
-        
-         
-       <AdminSidebar moduletypes={this.state.moduletypes} 
-          selectedtype={this.setModuleType}
-        />
-         <div className="flex flex-col bg-indblue min-h-full min-w-full">
+          <div className="flex flex-col min-h-screen">
             <div className="flex  flex-wrap">
-              <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
-              <WelcomeHero h1="ADD RESOURCES" />
-          <AdminResources />
-          <AdminResourceCard />
+              <AdminSidebar
+                moduletypes={this.state.moduletypes}
+                selectedtype={this.setModuleType}
+              />
+              <div className="flex flex-col bg-indblue min-h-full min-w-full">
+                <div className="flex  flex-wrap">
+                  <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
+                    <WelcomeHero h1="ADD RESOURCES" />
+                    <AdminResources />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-         
-       </div>
-   </div>
-
-       
         </>
       );
     }
     if (this.state.selectedmoduletype === 4) {
       return (
         <>
-            <div className="flex flex-col min-h-screen">
-       
-       <div className="flex  flex-wrap">
-        
-         
-       <AdminSidebar moduletypes={this.state.moduletypes} 
-          selectedtype={this.setModuleType}
-        />
-        <div className="flex flex-col bg-indblue min-h-full min-w-full">
+          <div className="flex flex-col min-h-screen">
             <div className="flex  flex-wrap">
-              <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
-              <WelcomeHero h1="ADD ANNOUNCEMENTS" />
-          <AdminAnnouncements />
-          <AdminResourceCard />
+              <AdminSidebar
+                moduletypes={this.state.moduletypes}
+                selectedtype={this.setModuleType}
+              />
+              <div className="flex flex-col bg-indblue min-h-full min-w-full">
+                <div className="flex  flex-wrap">
+                  <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
+                    <WelcomeHero h1="ADD ANNOUNCEMENTS" />
+                    <AdminAnnouncements />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-       </div>
-   </div>
-
-
-       
         </>
       );
     }
     if (this.state.selectedmoduletype === 5) {
       return (
         <>
-              <div className="flex flex-col min-h-screen">
-       
-       <div className="flex  flex-wrap">
-        
-         
-       <AdminSidebar moduletypes={this.state.moduletypes} 
-          selectedtype={this.setModuleType}
-        />
-          <div className="flex flex-col bg-indblue min-h-full min-w-full">
+          <div className="flex flex-col min-h-screen">
             <div className="flex  flex-wrap">
-              <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
-              <WelcomeHero h1="ADD USERS" />
-          <AdminUser />
-          <AdminResourceCard />
+              <AdminSidebar
+                moduletypes={this.state.moduletypes}
+                selectedtype={this.setModuleType}
+              />
+              <div className="flex flex-col bg-indblue min-h-full min-w-full">
+                <div className="flex  flex-wrap">
+                  <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
+                    <WelcomeHero h1="Import" />
+                    <AdminUser toast={toast} user={this.state.user}/>
+                  </div>
+                </div>
               </div>
             </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
           </div>
-         
-       </div>
-   </div>
-
-
-       
         </>
       );
-    }
-    else {
+    } else {
       return (
         <>
-          
-       
           <div className="flex flex-col min-h-screen">
-       
-       <div className="flex  flex-wrap">
-        
-         
-       <AdminSidebar moduletypes={this.state.moduletypes} 
-          selectedtype={this.setModuleType}
-        />
-        <div className="flex flex-col bg-indblue min-h-full min-w-full">
             <div className="flex  flex-wrap">
-              <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
-              <WelcomeHero h1="Welcome, " h2="Admin." />
-           <AdminStats />
-           <ChartStats />
+              <AdminSidebar
+                moduletypes={this.state.moduletypes}
+                selectedtype={this.setModuleType}
+              />
+              <div className="flex flex-col bg-indblue min-h-full min-w-full">
+                <div className="flex  flex-wrap">
+                  <div className="container md:rounded-tl-2xl min-h-screen bg-gray-100 md:ml-60 mt-14">
+                    <WelcomeHero h1="Welcome, " h2="Admin." />
+                    <AdminStats />
+                    <ChartStats />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-       </div>
-   </div>
-       
         </>
       );
     }
   }
-
 }
 export default Home;
-// export default function Home() {
-//   return (
-//     <div className="flex flex-col min-h-screen">
-//       <Head>
-//         <title>Create Next App</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <div className="flex  flex-wrap">
-//         <div className="container min-h-screen bg-gray-100 pt-0 md:ml-60">
-//           <WelcomeHero h1="Welcome, " h2="Bhuvanesh." />
-//           <AdminStats />
-//           <ChartStats />
-//         </div>
-//         <AdminSidebar />
-//       </div>
-//     </div>
-//   );
-// }
