69838e556384da9e4f448ad1708d63c22852db8f
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-04-25 19:32:33 +0530

    Update Design in Feedback

--- components/ProjectComponent.js
+++ components/ProjectComponent.js
@@ -140,73 +140,50 @@ class ProjectComponent extends Component {
         <>
           <div className="flex justify-between">
             <button
-              style={{ fontFamily: "Montserrat" }}
-              className="button-active bg-indblue p-5 m-3 text-white rounded-lg w-40 flex"
-              onClick={() => this.setProject(null)}
+                style={{fontFamily: "Montserrat"}}
+                className="button-active bg-indblue p-5 m-3 mb-16 text-white rounded-lg w-40 flex"
+                onClick={() => this.setProject(null)}
             >
               <svg
-                class="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
-                  d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
-                  clip-rule="evenodd"
+                    fill-rule="evenodd"
+                    d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+                    clip-rule="evenodd"
                 ></path>
-              </svg>{" "}
+              </svg>
+              {" "}
               <span className="ml-6">Back</span>
             </button>
             <div
-              style={{ fontFamily: "Montserrat" }}
-              className="bg-gray-200 p-3 rounded-xl m-3 ml-32 rounded-xl"
-            >
-              Initiative:{" "}
-              {
-                this.props.projects[this.state.selected_project].initiative
-                  .hackathon
-              }{" "}
-              -{" "}
-              {
-                this.props.projects[this.state.selected_project].initiative
-                  .edition
-              }
-              <br />
-              {this.props.projects[this.state.selected_project].ps_title !==
-              "NA"
-                ? `PS: ${
-                    this.props.projects[this.state.selected_project].ps_title
-                  }`
-                : ""}
-              <br />
-              <br />
-            </div>
-
-            <div
-              style={{ fontFamily: "Montserrat" }}
-              className="bg-gray-200 p-3 rounded-xl m-3 -ml-20 mr-20 rounded-xl"
+                style={{fontFamily: "Montserrat"}}
+                className="bg-gray-200 p-3 rounded-xl m-3 -ml-20 mr-20 rounded-xl"
             >
               {`Team: ${
-                this.props.projects[this.state.selected_project].team_name
+                  this.props.projects[this.state.selected_project].team_name
               }`}
-              <br />
+              <br/>
               {`Title: ${
-                this.props.projects[this.state.selected_project].title
+                  this.props.projects[this.state.selected_project].title
               }`}
-              <br />
+              <br/>
               {`College: ${
-                this.props.projects[this.state.selected_project].college
+                  this.props.projects[this.state.selected_project].college
               }`}
-              <br />
+              <br/>
               {`Role: ${
-                this.props.projects[this.state.selected_project].leader_id ==
-                this.props.user.id
-                  ? "Leader"
-                  : "Member"
+                  this.props.projects[this.state.selected_project].leader_id ==
+                  this.props.user.id
+                      ? "Leader"
+                      : "Member"
               }`}
-              <br />
+              <br/>
             </div>
+            <div className="col-span-6"></div>
           </div>
 
           <ProjectComponentTabs
