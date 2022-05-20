67e161e2a39674db8d7da8499734abf57b4ba81c
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-05-20 10:28:21 +0530

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import "tailwindcss/tailwind.css";
 import "@fontsource/montserrat/800.css";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+import {useRouter} from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID);
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ReactGA.pageview(url);
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />;
 }
 
