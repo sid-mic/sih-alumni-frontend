0a5e4c8ab5eaad121294b05b057dd3a3186dc330
sandippatil91 <ssandippatil91@gmail.com>
2022-05-31 15:19:52 +0530

    Design chagnes

--- components/Featured.js
+++ components/Featured.js
@@ -32,20 +32,12 @@ export default function Featured() {
   return (
     <div>
       <section>
-        <div class="px-4 md:mt-0 py-0 mx-auto max-w-screen-xl sm:px-6 lg:px-8 pt-2">
-          <div class="grid grid-cols-1 lg:grid-cols-2 gap-y-8 gap-x-16 lg:items-center">
+        <div class="px-4 md:mt-0 py-0 mx-auto max-w-screen-xl pt-2">
+          <div class="grid grid-cols-1">
             {stories?.mentor?.length > 2 && (
               <div class="relative">
                 {" "}
-                
-                {/*<h1 className={styles.mainHeading+" font-bold pb-2 mt-14 mb-14 sm:text-xl text-2xl md:text-3xl text-center"}>
-                  FEATURED 
-                  <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
-                    MENTORS
-                  </span>
-                  <div className={styles.bottomLine}></div>
-                </h1>*/}
-                <h1 className={styles.mainHeading+" font-bold pb-2 mt-14 mb-14 sm:text-xl text-2xl md:text-3xl text-center"}>
+                <h1 className={styles.mainHeading+" font-bold mb-16 mt-6 sm:text-xl text-2xl md:text-3xl text-center"}>
                   NOTABLE
                   <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
                     ALUMNI
@@ -53,26 +45,26 @@ export default function Featured() {
                   <div className={styles.bottomLine}></div>
                 </h1>
                 
-                <div class={styles.boxMentors+" flex flex-wrap -mx-3 overflow-hidden"}>
+                <div class={styles.boxMentors+" flex flex-wrap -mx-3 mt-10 pt-10"}>
                
                   {stories.mentor.map((alumni, index) => (
 
                     <div
-                      class="my-3 px-3 w-1/2 overflow-hidden lg:w-1/3"
+                      class="my-3 px-3 w-1/2 lg:w-1/3"
                       key={index}
                     >
                       <a
-                        class="relative block bg-black group rounded-xl"
+                        className={styles.outerImage+" relative block group"}
                         href={`/stories/${index + 4}`}
                       >
-                      <img className={styles.plusImageLeft} src="/assets/add.png" alt="img" />
+                      {/*<img className={styles.plusImageLeft} src="/assets/add.png" alt="img" />*/}
                         <img
-                          className="rounded-t-xl"
+                          className={styles.roundedImage}
                           src={alumni.user.picture}
                           alt={alumni.user.name}
                           style={{ height: 176, width: 176 }}
                         />
-                        <div class="relative rounded-b-xl overflow-hidden p-3 bg-indblue" style={{position:'relative'}}>
+                        <div className={styles.design+" relative rounded-b-xl overflow-hidden p-3 "} style={{position:'relative'}}>
                          
                            
                           <p class="text-sm text-left w-full font-bold text-white">
@@ -93,56 +85,7 @@ export default function Featured() {
                 </div>
               </div>
             )}
-
-            {stories?.alumni?.length > 2 && (
-              <div class="relative">
-                {" "}
-                
-                <h1 className={styles.mainHeading+" font-bold pb-2 mt-14 mb-14 sm:text-xl text-2xl md:text-3xl text-center"}>
-                  SUCCESS 
-                  <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
-                    STORIES
-                  </span>
-                  <div className={styles.bottomLine}></div>
-                </h1>
-                
-                <div class={styles.boxMentors+" flex flex-wrap -mx-3 overflow-hidden"}>
-  
-                  {stories.alumni.map((alumni, index) => (
-                    <div
-                      class="my-3 px-3 w-1/2 overflow-hidden lg:w-1/3"
-                      key={index}
-                    >
-                      <a
-                        class="relative block bg-black group rounded-xl"
-                        href={`/stories/${index + 1}`}
-                      >
-                       <img className={styles.plusImageLeft} src="/assets/add.png" alt="img" />
-                        <img
-                          className="rounded-t-xl"
-                          src={alumni.user.picture}
-                          alt={alumni.user.name}
-                          style={{ height: 176, width: 176 }}
-                        />
-                        <div class="relative rounded-b-xl overflow-hidden p-3 bg-indblue">
-                          <p class="text-sm text-left w-full font-bold text-white">
-                            {alumni.user.name}
-                          </p>
-                          <p class="text-sm text-left w-full font-bold text-white">
-                            {alumni.user.organization_name}
-                          </p>
-                          <p class="text-sm text-left w-full font-bold text-white">
-                            {alumni.user.designation}
-                          </p>
-                        </div>
-                        {/*TODO: FIX IMAGE HEIGHTS*/}
-                      </a>
-                    </div>
-                  ))}
-
-                </div>
-              </div>
-            )}
+          
           </div>
         </div>
       </section>
