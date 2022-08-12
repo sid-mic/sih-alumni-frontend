f19b41017e82b19264ef34e4a88598aee3c8b7dc
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-08-12 14:56:55 +0530

    Add city and state to mentor consent form

--- components/MentorWillingnessForm.js
+++ components/MentorWillingnessForm.js
@@ -290,6 +290,48 @@ export default function MentorWillingnessForm(props) {
               </div>
             </div>
           </div>
+
+          <div className="flex mb-5 -mx-3">
+            <div className="w-full px-3 mb-5">
+              <label className="text-md font-semibold">
+                5] Current Residing City:
+              </label>
+              <div className="flex">
+                <div className="w-10 z-10 pl-1  text-center pointer-events-none flex items-center justify-center">
+                  <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
+                </div>
+                <input
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                  disabled={disabled}
+                  type="text"
+                  className="w-full mt-5 -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 font-normal"
+                  defaultValue={mentor?.city}
+                  name={"city"}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex mb-5 -mx-3">
+            <div className="w-full px-3 mb-5">
+              <label className="text-md font-semibold">
+                5] Current Residing State:
+              </label>
+              <div className="flex">
+                <div className="w-10 z-10 pl-1  text-center pointer-events-none flex items-center justify-center">
+                  <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
+                </div>
+                <input
+                  style={{ fontFamily: "Poppins, sans-serif" }}
+                  disabled={disabled}
+                  type="text"
+                  className="w-full mt-5 -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 font-normal"
+                  defaultValue={mentor?.state}
+                  name={"state"}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       )}
 
