a08f5266bd6dd6253865e04d270c912972385e35
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-08-19 01:41:22 +0530

    Add mentor selection approval form

--- components/MentorWillingnessForm.js
+++ components/MentorWillingnessForm.js
@@ -6,92 +6,8 @@ import { toast } from "react-toastify";
 export default function MentorWillingnessForm(props) {
   let disabled = props.disabled ?? false;
 
-  let sw_nodal_centers = [
-    "ACS College of Engineering Karnataka Bengaluru",
-    "ANAND INSTITUTE OF HIGHER TECHNOLOGY Tamil Nadu CHENNAI",
-    "Vardhaman College of Engineering Telangana Hyderabad",
-    "Birla Institute of Technology Mesra Ranchi Jharkhand Ranchi",
-    "Cambridge Institute of Technology, K R Puram,Bangalore Karnataka Bengaluru",
-    "Siksha 'O' Anusandhan Odisha Bhubaneswar",
-    "Chandigarh University Punjab Mohali",
-    "D. Y. Patil College of Engineering, Akurdi, Pune Maharashtra Pune",
-    "DR B C ROY ENGINEERING COLLEGE West Bengal DURGAPUR",
-    "Excel Engineering College Tamil Nadu Komarapalayam",
-    "G H Raisoni College of Engineering, Nagpur Maharashtra Nagpur",
-    "G.PULLAIAH COLLEGE OF ENGINEERING AND TECHNOLOGY Andhra Pradesh KURNOOL",
-    "GIET University Odisha Gunupur",
-    "GMR Institute of Technology Andhra Pradesh Rajam",
-    "Gujarat Technological University Gujarat Ahmedabad",
-    "Chandigarh Engineering college-CGC Punjab Landran,Mohali",
-    "Hindusthan Institute of Technology, Coimbatore Tamil Nadu Coimbatore",
-    "IES College of Technology Madhya Pradesh Bhopal",
-    "IIT GUWAHATI Assam Guwahati",
-    "IIT KANPUR Uttar Pradesh KANPUR",
-    "IIT BHU Uttar Pradesh BANARAS",
-    "Jai Bharath College of Management and Engineering Technology Kerala Perumbavoor",
-    "JK Lakshmipat University Rajasthan Jaipur",
-    "JSS TECHNOLOGICAL UNIVERSITY Karnataka Mysore",
-    "Pandit Deendayal Energy University - PDEU Gujarat Gandhinagar",
-    "KIT'S College of Engineering Kolhapur Maharashtra Kolhapur",
-    "KLE Technological University Karnataka Hubballi",
-    "Koneru Lakshmaiah Education Foundation Andhra Pradesh Vijayawada",
-    "Kongu Engineering College Tamil Nadu Erode",
-    "KPR Institute of Engineering and Technology Tamil Nadu Coimbatore",
-    "Lovely Professional University Punjab Phagwara",
-    "Manipal University Jaipur Rajasthan Jaipur",
-    "Maharastra Institute of Technology, (MIT Aurangabad) Maharashtra Aurangabad",
-    "National Institute of Technology Silchar Assam Silchar",
-    "Noida Institute of Engineering and Technology, Greater Noida Uttar Pradesh Greater Noida",
-    "Paavai Engineering College Tamil Nadu Namakkal",
-    "Parul University Gujarat Vadodara",
-    "Presidency University, Bengaluru Karnataka Bengaluru",
-    "Prin. L.N. Welingkar Institute of Management Development and Research (PGDM) Maharashtra Mumbai",
-    "PSR ENGINEERING COLLEGE Tamil Nadu Sivakasi",
-    "IIT (ISM) Dhanbad Jharkhand Dhanbad",
-    "SAGE University Indore Madhya Pradesh Indore",
-    "Sahyadri College of Engineering & Management Karnataka Mangalore",
-    "Sathyabama Institute of Science and Tehcnology Tamil Nadu Chennai",
-    "SCMS School of Technology and Management Kerala Ernakulam",
-    "Shri Ramdeobaba College of Engineering and Management, Nagpur Maharashtra Nagpur",
-    "Sikkim Manipal Institute of Technology Sikkim RANGPO",
-    "Sona College of Technology Tamil Nadu Salem",
-    "SRI ESHWAR COLLEGE OF ENGINEERING Tamil Nadu COIMBATORE",
-    "Sri Shakthi Institute of Engineering & Technology Tamil Nadu Coimbatore",
-    "Sri Venkateshwaraa College of Engineering and Technology Puducherry Puducherry",
-    "Sri Venkateswara College of Engineering and Technology Andhra Pradesh Chittoor",
-    "Talla Padmavathi College of Engineering Telangana Warangal",
-    "Techno International New Town West Bengal Kolkata",
-    "Vaageswari College of Engineering Telangana Karimnagar",
-    "VIGNANA BHARATHI INSTITUTE OF TECHNOLOGY Telangana HYDERABAD",
-    "VNR VIGNANA JYOTHI INSTITUTE OF ENGINEERING & TECHNOLOGY Telangana HYDERABAD",
-  ];
-
-  let hw_nodal_centers = [
-    "A D Patel Institute of Technology Gujarat Anand",
-    "Centurion University of Technology and Management Odisha Bhubaneswar",
-    "Amal Jyothi College of Engineering Kerala Kanjirapally",
-    "B. S. Abdur Rahman Crescent Institute of Science & Technology Tamil Nadu Chennai",
-    "Bhilai Institute of Technology Chhattisgarh Durg",
-    "Chitkara University Punjab Rajpura",
-    "Forge Accelarator Tamil Nadu Coimbatore",
-    "Galgotias University Uttar Pradesh Greater Noida",
-    "Indian Institute of Technology Roorkee Uttarakhand Roorkee",
-    "JAIN (Deemed-to-be University)Faculty of Engineering and Technology Karnataka Bengaluru",
-    "Kalasalingam Academy of Research and Education Tamil Nadu Srivilliputtur",
-    "KIET Group of Institutions Ghaziabad Uttar Pradesh Ghaziabad",
-    "Manav Rachna International Institute of Research and Studies Haryana Faridabad",
-    "Arya Institute of Engineering and Technology Rajasthan Jaipur",
-    "MIT PUNE Maharashtra PUNE",
-    "QIS COLLEGE OF ENGINEERING AND TECHNOLOGY Andhra Pradesh ONGOLE",
-    "REVA University Karnataka Bengaluru",
-    "SRM Institute of Science & Technology Tamil Nadu Chennai",
-  ];
-
   const [mentor, setMentor] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-
-  const [interested, setInterested] = useState(true);
-  const [category, setCategory] = useState("sw");
 
   useEffect(() => {
     axios()
@@ -102,11 +18,13 @@ export default function MentorWillingnessForm(props) {
       )
       .then((response) => {
         setMentor(response.data);
-        setInterested(response.data?.interested);
-        setCategory(response.data?.category);
         setIsLoading(false);
       });
   }, []);
+
+  if (!mentor || mentor?.interested == 0) {
+    return <h5 className="mt-12 text-center">Not interested</h5>;
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -163,192 +81,84 @@ export default function MentorWillingnessForm(props) {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="ml:0 md:ml-20 mr-0 md:mr-18 lg:mr-20 mt-3"
-    >
-      <div className="flex -mx-3">
-        <div className="w-full px-3 mb-5">
-          <label className="text-md font-semibold">
-            1] Are you interested in mentoring for Smart India Hackathon 2022?
-          </label>
-          <div className="main xl:flex m-2 select-none">
-            <label className="flex radio p-2 cursor-pointer">
-              <input
-                required={true}
-                name="interested"
-                disabled={disabled}
-                className="my-auto transform scale-125"
-                type="radio"
-                value={1}
-                defaultChecked={interested === 1}
-                onClick={(e) => setInterested(1)}
-              />
-              <div className="title px-2">Interested</div>
-            </label>
-
-            <label className="flex radio p-2 cursor-pointer">
-              <input
-                required={true}
-                name="interested"
-                disabled={disabled}
-                className="my-auto transform scale-125"
-                type="radio"
-                value={0}
-                defaultChecked={interested === 0}
-                onClick={(e) => setInterested(0)}
-              />
-              <div className="title px-2">Not Interested</div>
-            </label>
-          </div>
-        </div>
-      </div>
-
-      {interested === 1 && (
+    <div className="text-xl font-bold">
+      {!mentor.is_selected ? (
+        <h5 className="mt-12 text-center">
+          Thanks for your interest in participating SIH 2022. <br/> But unfortunately,
+          due to limited vacancies, you are not selected.
+        </h5>
+      ) : (
         <div>
-          <div className="flex -mx-3">
-            <div className="w-full px-3 mb-5">
-              <label className="text-md font-semibold">
-                2] Select your preferred category:
-              </label>
-              <div className="main xl:flex m-2 select-none">
-                <label className="flex radio p-2 cursor-pointer">
-                  <input
-                    required={true}
-                    name="category"
-                    disabled={disabled}
-                    className="my-auto transform scale-125"
-                    type="radio"
-                    value={"sw"}
-                    defaultChecked={category === "sw"}
-                    onClick={(e) => setCategory(e.target.value)}
-                  />
-                  <div className="title px-2">Software</div>
-                </label>
+          <h3 className="mt-4 text-center">
+            Congratulations! You have been selected as {mentor.associate} in
+            {mentor.category === "sw" ? "Software" : "Hardware"} category in SIH
+            2022. <br /> Nodal center alloted to you is {mentor.nodal_center}.
+             <br/><br/>{mentor.category === 'sw' ? "SIH 2022 Software edition is scheduled between 25th and 26th August 2022." : "SIH 2022 Hardware edition is scheduled between 25 to 29th August 2022."}
+            <br/>
 
-                <label className="flex radio p-2 cursor-pointer">
-                  <input
-                    required={true}
-                    name="category"
-                    disabled={disabled}
-                    className="my-auto transform scale-125"
-                    type="radio"
-                    value={"hw"}
-                    defaultChecked={category === "hw"}
-                    onClick={(e) => setCategory(e.target.value)}
-                  />
-                  <div className="title px-2">Hardware</div>
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex mb-5 -mx-3">
-            <div className="w-full px-3 mb-5">
-              <label className="text-md font-semibold">
-                3] Select your preferred nodal center:
-              </label>
-              <div className="flex">
-                <select
-                  disabled={disabled}
-                  className="mt-5 block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  defaultValue={mentor?.nodal_center}
-                  name="nodal_center"
-                >
-                  {category === "sw"
-                    ? sw_nodal_centers.map((n, index) => (
-                        <option value={n} key={index}>
-                          {n}
-                        </option>
-                      ))
-                    : hw_nodal_centers.map((n, index) => (
-                        <option value={n} key={index}>
-                          {n}
-                        </option>
-                      ))}
-                </select>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex mb-5 -mx-3">
-            <div className="w-full px-3 mb-5">
-              <label className="text-md font-semibold">
-                4] Associate yourself as:
-              </label>
-              <div className="flex">
-                <select
-                  disabled={disabled}
-                  className="mt-5 block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  defaultValue={mentor?.associate}
-                  name="associate"
-                >
-                  <option value="Evaluator">Evaluator</option>
-                  <option value="Mentor">Mentor</option>
-                  <option value="Design Expert">Design Expert</option>
-                </select>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex mb-5 -mx-3">
-            <div className="w-full px-3 mb-5">
-              <label className="text-md font-semibold">
-                5] Current residing city (From where your will travel to the SIH
-                2022 nodal centre):
-              </label>
-              <div className="flex">
-                <div className="w-10 z-10 pl-1  text-center pointer-events-none flex items-center justify-center">
-                  <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
-                </div>
-                <input
-                  style={{ fontFamily: "Poppins, sans-serif" }}
-                  disabled={disabled}
-                  type="text"
-                  className="w-full mt-5 -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 font-normal"
-                  defaultValue={mentor?.city}
-                  name={"city"}
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="flex mb-5 -mx-3">
-            <div className="w-full px-3 mb-5">
-              <label className="text-md font-semibold">
-                6] Current residing state (From where your will travel to the
-                SIH2022 nodal centre):
-              </label>
-              <div className="flex">
-                <div className="w-10 z-10 pl-1  text-center pointer-events-none flex items-center justify-center">
-                  <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
-                </div>
-                <input
-                  style={{ fontFamily: "Poppins, sans-serif" }}
-                  disabled={disabled}
-                  type="text"
-                  className="w-full mt-5 -ml-10 pl-4 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500 font-normal"
-                  defaultValue={mentor?.state}
-                  name={"state"}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {!disabled && (
-        <div className="flex -mx-3">
-          <div className="w-full px-3 mb-5">
-            <button
-              style={{ fontFamily: "Montserrat" }}
-              className="block w-full max-w-xs mx-auto bg-indblue hover:bg-indblue focus:bg-indblue text-white rounded-lg px-3 py-3 font-semibold"
+          </h3>
+          {mentor.is_accepted === null ? (
+            <form
+              onSubmit={handleSubmit}
+              className="ml:0 md:ml-20 mr-0 md:mr-18 lg:mr-20 mt-10 text-center"
             >
-              {isLoading ? "Saving...." : "SAVE CHANGES"}
-            </button>
-          </div>
+              <div className="flex -mx-3">
+                <div className="w-full px-3 mb-5">
+                  <label className="text-md font-semibold">
+                    Do you confirm your presence in SIH 2022?{" "}
+                    <span className="text-red-600">
+                      (This form can be submitted only once)<sup>*</sup>
+                    </span>
+                  </label>
+                  <div className="main xl:flex m-2 select-none justify-center">
+                    <label className="flex radio p-2 cursor-pointer">
+                      <input
+                        required={true}
+                        name="is_accepted"
+                        disabled={disabled}
+                        className="my-auto transform scale-125"
+                        type="radio"
+                        value={1}
+                      />
+                      <div className="title px-2">Confirm my presence</div>
+                    </label>
+
+                    <label className="flex radio p-2 cursor-pointer">
+                      <input
+                        required={true}
+                        name="is_accepted"
+                        disabled={disabled}
+                        className="my-auto transform scale-125"
+                        type="radio"
+                        value={0}
+                      />
+                      <div className="title px-2">Not Interested</div>
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              {!disabled && (
+                <div className="flex -mx-3">
+                  <div className="w-full px-3 mb-5">
+                    <button
+                      style={{ fontFamily: "Montserrat" }}
+                      className="block w-full max-w-xs mx-auto bg-indblue hover:bg-indblue focus:bg-indblue text-white rounded-lg px-3 py-3 font-semibold"
+                    >
+                      {isLoading ? "Saving...." : "SAVE CHANGES"}
+                    </button>
+                  </div>
+                </div>
+              )}
+            </form>
+          ) : (
+            <h5 className="mt-4 text-center">
+              You {mentor.is_accepted  != "0" ? "have accepted" : "are not interested"}{" "}
+              to participate.
+            </h5>
+          )}
         </div>
       )}
-    </form>
+    </div>
   );
 }
