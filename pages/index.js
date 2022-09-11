382e36508d742d2dafb58a606e2cad5c82609e1e
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-09-11 19:54:43 +0530

    Comment graph in master

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
             <Feature />
             
             <Who />
-            <Graph />
+            {/*<Graph />*/}
             <Benefits />
             <News/>
             <Featured />
